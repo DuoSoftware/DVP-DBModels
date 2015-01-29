811563941a26155e57d5201b8787c7af08958d14
dinusha_dck <dinusha@duosoftware.com>
2015-01-29 15:26:13 +0530

    Commit Initial Model

---
+++ Context.js
@@ -0,0 +1,23 @@
+module.exports = function(sequelize, DataTypes) {
+    var Context = sequelize.define('CSDB_Context', {
+            Context: DataTypes.STRING,
+            Description: DataTypes.STRING,
+            ContextCat: DataTypes.STRING,
+            CompanyId: DataTypes.INTEGER,
+            TenantId: DataTypes.INTEGER,
+            ObjClass: DataTypes.STRING,
+            ObjType: DataTypes.STRING,
+            ObjCategory: DataTypes.STRING,
+            AddUser: DataTypes.STRING,
+            UpdateUser: DataTypes.STRING,
+            AddTime: DataTypes.DATE,
+            UpdateTime: DataTypes.DATE
+        },{
+            tableName: 'CSDB_Context', // this will define the table's name
+            timestamps: false           // this will deactivate the timestamp columns
+        }
+    );
+
+
+    return Context;
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+var Sequelize = require('sequelize');
+var Config = require('config');
+
+var dbType = Config.DB.Type;
+var database = Config.DB.Database;
+var dbuser = Config.DB.User;
+var dbpassword = Config.DB.Password;
+var dbport = Config.DB.Port;
+var dbhost = Config.DB.Host;
+
+var sequelize = new Sequelize(database, dbuser, dbpassword, {
+    dialect:dbType, // or 'sqlite', 'postgres', 'mariadb'
+    port:dbport, // or 5432 (for postgres)
+    host:dbhost //host address
+});
+
+var models = [
+    'Context'
+];
+
+models.forEach(function(model) {
+    module.exports[model] = sequelize.import(__dirname + '/DVP-DBModels/' + model);
+});
+module.exports.SequelizeConn = sequelize;

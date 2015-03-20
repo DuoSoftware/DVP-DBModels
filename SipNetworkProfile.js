0fdd96d5f6931fb303112b50b19158f44626b96f
sukitha <sukithaj@gmail.com>
2015-03-20 16:50:01 +0530

    network profile composite key added

--- SipNetworkProfile.js
+++ SipNetworkProfile.js
@@ -2,11 +2,11 @@ module.exports = function(sequelize, DataTypes) {
     var SipNetworkProfile = sequelize.define('CSDB_SipNetworkProfile', {
             MainIp: DataTypes.STRING,
             ProfileName: DataTypes.STRING,
-            InternalIp: DataTypes.STRING,
+            InternalIp: {type: DataTypes.STRING,unique: "compositeIndex"},
             InternalRtpIp: DataTypes.STRING,
             ExternalIp: DataTypes.STRING,
             ExternalRtpIp: DataTypes.STRING,
-            Port: DataTypes.INTEGER,
+            Port: {type: DataTypes.INTEGER,unique: "compositeIndex"},
             ObjClass: DataTypes.STRING,
             ObjType: DataTypes.STRING,
             ObjCategory: DataTypes.STRING,

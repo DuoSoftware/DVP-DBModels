2cd4dc7a0f25ebe65564b2161c5b713f7664791a
dinusha_dck <dinusha@duosoftware.com>
2015-03-23 12:39:06 +0530

    Add Translation and Schedule Association to CR

--- index.js
+++ index.js
@@ -68,7 +68,9 @@ models.forEach(function(model) {
     m.CloudEndUser.belongsTo(m.Network, {as: "Networks"});
     //m.CloudEndUser.belongsToMany(m.SipNetworkProfile, {as:"SipNetworkProfiles"});
     m.CloudEndUser.belongsTo(m.SipNetworkProfile, {as:"SipNetworkProfiles"});
-    m.IPAddress.belongsTo(m.CallServer, {as: "CallServer"})
+    m.IPAddress.belongsTo(m.CallServer, {as: "CallServer"});
+    m.Schedule.hasMany(m.CallRule, {as: "CallRules"});
+    m.Translation.hasMany(m.CallRule, {as: "CallRules"});
 
 
 

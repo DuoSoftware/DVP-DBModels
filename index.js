d65c003dec8ff1a758bb8f702b4b7b6842263b7c
dinusha_dck <dinusha@duosoftware.com>
2015-03-31 16:24:46 +0530

    All model associations wired

--- index.js
+++ index.js
@@ -49,35 +49,69 @@ models.forEach(function(model) {
     //m.CloudEndUser.belongsTo(m.Cloud);
     //m.Network.belongsTo(m.CloudEndUser);
     m.TrunkPhoneNumber.belongsTo(m.Schedule, {as:"Schedule", foreignKey:"ScheduleId"});
+    m.Schedule.hasMany(m.TrunkPhoneNumber, {as:"TrunkPhoneNumber", foreignKey:"ScheduleId"});
+
     m.Trunk.hasMany(m.TrunkPhoneNumber, {as:"TrunkPhoneNumbers", foreignKey: "TrunkId"});
+    m.TrunkPhoneNumber.belongsTo(m.Trunk, {as:"Trunk", foreignKey: "TrunkId"});
+
     m.TrunkPhoneNumber.belongsTo(m.LimitInfo, {as:"LimitInfo", foreignKey: "LimitId"});
+    m.LimitInfo.hasMany(m.TrunkPhoneNumber, {as:"TrunkPhoneNumber", foreignKey: "LimitId"});
+
     m.UserGroup.belongsTo(m.Extension, {as:"Extension", foreignKey: "ExtensionId"});
+    m.Extension.hasOne(m.UserGroup, {as:"UserGroup", foreignKey: "ExtensionId"});
+
     m.UserGroup.belongsToMany(m.SipUACEndpoint, {through: 'CSDB_UsrGrpJunction'});
     m.SipUACEndpoint.belongsToMany(m.UserGroup, {through: 'CSDB_UsrGrpJunction'});
+
     m.Schedule.hasMany(m.Appointment, {as:"Appointment", foreignKey: "ScheduleId"});
+    m.Appointment.belongsTo(m.Schedule, {as:"Schedule", foreignKey: "ScheduleId"});
+
     m.CallServer.hasMany(m.SipNetworkProfile, {as: "SipNetworkProfile", foreignKey: "CallServerId"});
     m.SipNetworkProfile.belongsTo(m.CallServer, {as: "CallServer", foreignKey: "CallServerId"});
+
     m.SipUACEndpoint.belongsTo(m.Extension, {as:"Extension", foreignKey: "ExtensionId"});
+    m.Extension.hasOne(m.SipUACEndpoint, {as:"SipUACEndpoint", foreignKey: "ExtensionId"});
+
     m.Trunk.belongsTo(m.SipNetworkProfile, {as:"SipNetworkProfile", foreignKey:"ProfileId"});
     m.SipNetworkProfile.hasMany(m.Trunk, {as:"Trunk", foreignKey:"ProfileId"});
-    m.Context.hasMany(m.SipUACEndpoint, {as:"SipUACEndpoint", foreignKey:"Context"});
+
+    m.Context.hasMany(m.SipUACEndpoint, {as:"SipUACEndpoint", foreignKey:"ContextId"});
+    m.SipUACEndpoint.belongsTo(m.Context, {as:"Context", foreignKey:"ContextId"});
+
     m.Trunk.belongsTo(m.LoadBalancer, {as: "LoadBalancer", foreignKey: "LoadBalancerId"});
+    m.LoadBalancer.hasMany(m.Trunk, {as: "Trunk", foreignKey: "LoadBalancerId"});
+
     m.Cloud.belongsTo(m.LoadBalancer,{as: "LoadBalancer", foreignKey: "LoadBalancerId"});
+    m.LoadBalancer.hasOne(m.Cloud,{as: "Cloud", foreignKey: "LoadBalancerId"});
+
     m.CloudEndUser.hasMany(m.SipUACEndpoint, {as: "SipUACEndpoint", foreignKey: "CloudEndUserId"});
+    m.SipUACEndpoint.belongsTo(m.CloudEndUser, {as: "CloudEndUser", foreignKey: "CloudEndUserId"});
+
     m.Cloud.hasMany(m.CallServer, {as: "CallServer", foreignKey: "ClusterId"});
     m.CallServer.belongsTo(m.Cloud, {as: "Cloud", foreignKey: "ClusterId"});
+
     m.Cloud.hasMany(m.Network, {as: "Network", foreignKey: "ClusterId"});
+    m.Network.belongsTo(m.Cloud, {as: "Cloud", foreignKey: "ClusterId"});
+
     m.Cloud.hasMany(m.CloudEndUser, {as: "CloudEndUser", foreignKey: "ClusterId"});
     m.CloudEndUser.belongsTo(m.Cloud, {as: "Cloud", foreignKey: "ClusterId"});
+
     m.Cloud.belongsTo(m.Cloud, {as: "ParentCloud"});
+
     m.CloudEndUser.belongsTo(m.Network, {as: "Network", foreignKey: "NetworkId"});
-    //m.CloudEndUser.belongsToMany(m.SipNetworkProfile, {as:"SipNetworkProfiles"});
+    m.Network.hasOne(m.CloudEndUser, {as: "CloudEndUser", foreignKey: "NetworkId"});
+
     m.CloudEndUser.belongsTo(m.SipNetworkProfile, {as:"SipNetworkProfile", foreignKey:"SipNetworkProfileId"});
     m.SipNetworkProfile.hasOne(m.CloudEndUser, {as:"CloudEndUser", foreignKey:"SipNetworkProfileId"});
-    m.IPAddress.belongsTo(m.CallServer, {as: "CallServer", foreignKey: "CallServerId"});
-    m.Schedule.hasMany(m.CallRule, {as: "CallRule", foreignKey: "ScheduleId"});
-    m.Translation.hasMany(m.CallRule, {as: "CallRule", foreignKey: "TranslationId"});
 
+    m.IPAddress.belongsTo(m.CallServer, {as: "CallServer", foreignKey: "CallServerId"});
+    m.CallServer.hasMany(m.IPAddress, {as: "IPAddress", foreignKey: "CallServerId"});
+
+    m.Schedule.hasMany(m.CallRule, {as: "CallRule", foreignKey: "ScheduleId"});
+    m.CallRule.belongsTo(m.Schedule, {as: "Schedule", foreignKey: "ScheduleId"});
+
+    m.Translation.hasMany(m.CallRule, {as: "CallRule", foreignKey: "TranslationId"});
+    m.CallRule.belongsTo(m.Translation, {as: "Translation", foreignKey: "TranslationId"});
 
 
 })(module.exports);

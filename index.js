64207f867057529ba991ef6784e77abaae6933e8
Pawan Sasanka <pawan@duosoftware.com>
2015-04-10 10:38:02 +0530

    Relationship changed(app and upload)

--- index.js
+++ index.js
@@ -131,8 +131,9 @@ models.forEach(function(model) {
     m.Application.belongsTo(m.AppDeveloper, {as: "AppDeveloper", foreignKey: "AppDeveloperId"});
     m.AppDeveloper.hasMany(m.Application, {as: "Application", foreignKey: "AppDeveloperId"});
 
-    m.Application.belongsToMany(m.FileUpload, {as: "FileUpload", through: 'CSDB_AppVoiceUploadJunction'});
-    m.FileUpload.belongsToMany(m.Application, {as: "Application", through: 'CSDB_AppVoiceUploadJunction'});
+
+    m.FileUpload.belongsTo(m.Application, {as: "Application", foreignKey: 'ApplicationId'});
+    m.Application.hasMany(m.FileUpload, {as: "FileUpload", foreignKey: "ApplicationId"});
 
 
 })(module.exports);

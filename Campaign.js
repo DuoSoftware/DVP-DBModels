7665360ebcc95c502b25aa531d214b0193350104
Pawan Sasanka <pawan@duosoftware.com>
2015-06-03 07:13:27 +0530

    New modules added

---
+++ Campaign.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Pawan on 6/1/2015.
+ */
+module.exports = function(sequelize, DataTypes) {
+    var Campaign = sequelize.define('CSDB_Campaign', {
+            CampaignName: DataTypes.STRING,
+            CampaignNumber: DataTypes.STRING,
+            Max:DataTypes.INTEGER,
+            Min:DataTypes.INTEGER,
+            CompanyId: DataTypes.INTEGER,
+            TenantId: DataTypes.INTEGER,
+            Class: DataTypes.STRING,
+            Type: DataTypes.STRING,
+            Category: DataTypes.STRING,
+            StartTime: DataTypes.STRING,
+            EndTime:DataTypes.INTEGER,
+            Enable:DataTypes.BOOLEAN,
+            ScheduleId: DataTypes.STRING,
+            Limit:DataTypes.INTEGER
+
+
+        }
+    );
+
+
+    return Campaign;
+};

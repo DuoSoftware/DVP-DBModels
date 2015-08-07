be62863c1c70b19b1ed908cfe204db4d0ca2f8e1
Pawan Sasanka <pawan@duosoftware.com>
2015-08-07 15:10:33 +0530

    Fixed Branch shifting bugs

--- CampCampaignInfo.js
+++ CampCampaignInfo.js
@@ -3,45 +3,39 @@
  */
 
 
-<<<<<<< HEAD
+
 module.exports = function (sequelize, DataTypes) {
     var CampCampaignInfo = sequelize.define('DB_CAMP_CampaignInfo', {
-            CampaignName: {type: DataTypes.STRING, allowNull: false, unique: true},
-            CampaignMode: DataTypes.STRING,
-            CampaignChannel: DataTypes.STRING,
-            DialoutMechanism: DataTypes.STRING,
-            TenantId: DataTypes.INTEGER,
-=======
+        CampaignName: {type: DataTypes.STRING, allowNull: false, unique: true},
+        CampaignMode: DataTypes.STRING,
+        CampaignChannel: DataTypes.STRING,
+        DialoutMechanism: DataTypes.STRING,
+        TenantId: DataTypes.INTEGER
+    });
+}
+
+
 module.exports = function(sequelize, DataTypes) {
     var CampCampaignInfo = sequelize.define('DB_CAMP_CampaignInfo', {
             CampaignName: DataTypes.STRING,
-<<<<<<< HEAD
-<<<<<<< HEAD
             CampaignnMode: DataTypes.STRING,
-=======
             CampaignMode: DataTypes.STRING,
->>>>>>> master
-=======
             CampaignnMode: DataTypes.STRING,
->>>>>>> f22701c3c07b971a81951fd6e7efebd46bd4b7f0
             CampaignChannel:DataTypes.STRING,
             DialoutMechanism:DataTypes.STRING,
             TenantId:DataTypes.INTEGER,
->>>>>>> 1a3f0fd2e03012fc947fcc52bd6962245927e56e
             CompanyId: DataTypes.INTEGER,
             Class: DataTypes.STRING,
             Type: DataTypes.STRING,
             Category: DataTypes.STRING,
-<<<<<<< HEAD
             Extensions: DataTypes.STRING,
             //Concurrent: DataTypes.STRING,
             OperationalStatus: DataTypes.STRING,
             Status: DataTypes.BOOLEAN,
-            CampaignId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
-=======
+            CampaignId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
             CampaignId: DataTypes.INTEGER,
             Status:DataTypes.BOOLEAN
->>>>>>> 1a3f0fd2e03012fc947fcc52bd6962245927e56e
+
         }
     );
 
--- CampScheduleInfo.js
+++ CampScheduleInfo.js
@@ -3,9 +3,7 @@
  */
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 module.exports = function (sequelize, DataTypes) {
     var CampScheduleInfo = sequelize.define('DB_CAMP_ScheduleInfo', {
             CampaignId: {type:DataTypes.INTEGER,unique: 'CampScheduleInfoIndex'},
@@ -15,18 +13,19 @@ module.exports = function (sequelize, DataTypes) {
             TenantId: DataTypes.INTEGER,
             CompanyId: DataTypes.INTEGER,
             Status: DataTypes.BOOLEAN,
-            CamScheduleId:{type:DataTypes.INTEGER, primaryKey:true,autoIncrement: true}
-=======
->>>>>>> f22701c3c07b971a81951fd6e7efebd46bd4b7f0
+            CamScheduleId:{type:DataTypes.INTEGER, primaryKey:true,autoIncrement: true}})
+}
+
 module.exports = function(sequelize, DataTypes) {
-    var CampScheduleInfo = sequelize.define('DB_CAMP_ScheduleInfo', {
-            CampaignId: DataTypes.INTEGER,
-            ScheduleId: DataTypes.INTEGER,
-            ScheduleType:DataTypes.BOOLEAN,
-            CamScheduleId:DataTypes.INTEGER,
-            Status:DataTypes.BOOLEAN
-<<<<<<< HEAD
-=======
+        var CampScheduleInfo = sequelize.define('DB_CAMP_ScheduleInfo', {
+                CampaignId: DataTypes.INTEGER,
+                ScheduleId: DataTypes.INTEGER,
+                ScheduleType: DataTypes.BOOLEAN,
+                CamScheduleId: DataTypes.INTEGER,
+                Status: DataTypes.BOOLEAN
+        });
+}
+
 module.exports = function (sequelize, DataTypes) {
     var CampScheduleInfo = sequelize.define('DB_CAMP_ScheduleInfo', {
             CampaignId: DataTypes.INTEGER,
@@ -36,10 +35,7 @@ module.exports = function (sequelize, DataTypes) {
             TenantId: DataTypes.INTEGER,
             CompanyId: DataTypes.INTEGER,
             Status: DataTypes.BOOLEAN
->>>>>>> master
-=======
->>>>>>> 1a3f0fd2e03012fc947fcc52bd6962245927e56e
->>>>>>> f22701c3c07b971a81951fd6e7efebd46bd4b7f0
+
         }
     );
     return CampScheduleInfo;

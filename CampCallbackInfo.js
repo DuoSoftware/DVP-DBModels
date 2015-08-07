e29f23cf11a08ff84783b6f5e79710239dd04abc
Pawan Sasanka <pawan@duosoftware.com>
2015-08-07 15:22:43 +0530

    Fixed

--- CampCallbackInfo.js
+++ CampCallbackInfo.js
@@ -1,23 +1,15 @@
-/**
- * Created by Rajinda on 6/29/2015.
- */
-
-
-
 
 module.exports = function(sequelize, DataTypes) {
     var CampCallbackInfo = sequelize.define('DB_CAMP_CallbackInfo', {
             CampaignId: DataTypes.INTEGER,
             ContactId: DataTypes.STRING,
             CamScheduleId:DataTypes.INTEGER,
-<<<<<<< HEAD
             //CallBackId:DataTypes.INTEGER,
             Status:DataTypes.BOOLEAN,
-            CallBackId:{type:DataTypes.INTEGER, primaryKey:true,autoIncrement: true}
-=======
+            CallBackId:{type:DataTypes.INTEGER, primaryKey:true,autoIncrement: true},
             CallBackId:DataTypes.STRING,
             Status:DataTypes.BOOLEAN
->>>>>>> 1a3f0fd2e03012fc947fcc52bd6962245927e56e
+
 
         }
     );
--- CampConfigurations.js
+++ CampConfigurations.js
@@ -7,33 +7,28 @@ module.exports = function(sequelize, DataTypes) {
     var CampConfigurations = sequelize.define('DB_CAMP_Configurations', {
             CampaignId: DataTypes.INTEGER,
             ChannelConcurrency: DataTypes.INTEGER,
-            AllowCallBack:DataTypes.BOOLEAN,
-            MaxCallBackCount:DataTypes.INTEGER,
-<<<<<<< HEAD
-            ConfigureId:DataTypes.BOOLEAN,
-<<<<<<< HEAD
-            Status:DataTypes.BOOLEAN
-=======
-            Status:DataTypes.BOOLEAN,
-            TenantId:DataTypes.INTEGER,
-            CompanyId:DataTypes.INTEGER
->>>>>>> master
-=======
-<<<<<<< HEAD
+            AllowCallBack: DataTypes.BOOLEAN,
+            MaxCallBackCount: DataTypes.INTEGER,
+            ConfigureId: DataTypes.BOOLEAN,
+
+            Status: DataTypes.BOOLEAN,
+
+            Status: DataTypes.BOOLEAN,
+            TenantId: DataTypes.INTEGER,
+            CompanyId: DataTypes.INTEGER,
+
             Caller: DataTypes.STRING,
             //ConfigureId:DataTypes.INTEGER,
-            Status:DataTypes.BOOLEAN,
-            TenantId:DataTypes.INTEGER,
-            CompanyId:DataTypes.INTEGER,
-            StartDate:DataTypes.DATE,
-            EndDate:DataTypes.DATE,
-            ConfigureId:{type:DataTypes.INTEGER, primaryKey:true,autoIncrement: true}
-=======
-            ConfigureId:DataTypes.BOOLEAN,
-            Status:DataTypes.BOOLEAN
->>>>>>> 1a3f0fd2e03012fc947fcc52bd6962245927e56e
->>>>>>> f22701c3c07b971a81951fd6e7efebd46bd4b7f0
+            Status: DataTypes.BOOLEAN,
+            TenantId: DataTypes.INTEGER,
+            CompanyId: DataTypes.INTEGER,
+            StartDate: DataTypes.DATE,
+            EndDate: DataTypes.DATE,
+
+            ConfigureId: DataTypes.BOOLEAN,
+            Status: DataTypes.BOOLEAN
         }
+
     );
     return CampConfigurations;
 };

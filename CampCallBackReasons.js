1881e48e77b34e379d40709802d9eefe2fa62994
waruna <waruna@duosoftware.com>
2015-08-10 10:21:31 +0530

    add 2files

---
+++ CampCallBackReasons.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Rajinda on 8/7/2015.
+ */
+
+
+module.exports = function(sequelize, DataTypes) {
+    var CampCallBackReasons = sequelize.define('DB_CAMP_CallBackReasons', {
+            Reason: {type:DataTypes.STRING,unique: 'CallBackReasonsCompositeIndex'},
+            TenantId:{type:DataTypes.INTEGER,unique: 'CallBackReasonsCompositeIndex'},
+            CompanyId:{type:DataTypes.INTEGER,unique: 'CallBackReasonsCompositeIndex'},
+            ReasonId:{type:DataTypes.INTEGER, primaryKey:true,autoIncrement: true}
+        }
+    );
+    return CampCallBackReasons;
+};
+

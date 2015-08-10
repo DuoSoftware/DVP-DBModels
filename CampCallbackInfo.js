8049107b0ba4b807083b80b88dcd6b5aa3975e8d
waruna <waruna@duosoftware.com>
2015-08-10 11:12:01 +0530

    add field

--- CampCallbackInfo.js
+++ CampCallbackInfo.js
@@ -7,10 +7,10 @@
 
 module.exports = function(sequelize, DataTypes) {
     var CampCallbackInfo = sequelize.define('DB_CAMP_CallbackInfo', {
-            CampaignId: DataTypes.INTEGER,
-            ContactId: DataTypes.STRING,
-            CamScheduleId:DataTypes.INTEGER,
-            //CallBackId:DataTypes.INTEGER,
+            CampaignId: {type:DataTypes.INTEGER,unique: 'CampCallbackInfoCompositeIndex'},
+            ContactId: {type:DataTypes.STRING,unique: 'CampCallbackInfoCompositeIndex'},
+            CamScheduleId:{type:DataTypes.INTEGER,unique: 'CampCallbackInfoCompositeIndex'},
+            CallBackCount:DataTypes.INTEGER,
             Status:DataTypes.BOOLEAN,
             CallBackId:{type:DataTypes.INTEGER, primaryKey:true,autoIncrement: true}
 

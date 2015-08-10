450ff9e0b23adb8a61e25b98c2234a4cbced4d94
dinusha_dck <dinusha@duosoftware.com>
2015-08-10 15:19:36 +0530

    add attendant transfer feature codes

--- SipUACEndpoint.js
+++ SipUACEndpoint.js
@@ -1,23 +1,28 @@
 module.exports = function(sequelize, DataTypes) {
     var SipUACEndpoint = sequelize.define('CSDB_SipUACEndpoint', {
-            SipUserUuid: DataTypes.STRING,
-            SipUsername: DataTypes.STRING,
-            Password: DataTypes.STRING,
-            Enabled: DataTypes.BOOLEAN,
-            ExtraData: DataTypes.STRING,
-            EmailAddress: DataTypes.STRING,
-            GuRefId: DataTypes.STRING,
-            CompanyId: DataTypes.INTEGER,
-            TenantId: DataTypes.INTEGER,
-            ObjClass: DataTypes.STRING,
-            ObjType: DataTypes.STRING,
-            ObjCategory: DataTypes.STRING,
-            SipExtension: DataTypes.STRING,
-            AddUser: DataTypes.STRING,
-            Pin:DataTypes.STRING,
-            PinGenTime:DataTypes.DATE,
-            TryCount:DataTypes.INTEGER,
-            UpdateUser: DataTypes.STRING
+                SipUserUuid: DataTypes.STRING,
+                SipUsername: DataTypes.STRING,
+                Password: DataTypes.STRING,
+                Enabled: DataTypes.BOOLEAN,
+                ExtraData: DataTypes.STRING,
+                EmailAddress: DataTypes.STRING,
+                GuRefId: DataTypes.STRING,
+                CompanyId: DataTypes.INTEGER,
+                TenantId: DataTypes.INTEGER,
+                ObjClass: DataTypes.STRING,
+                ObjType: DataTypes.STRING,
+                ObjCategory: DataTypes.STRING,
+                SipExtension: DataTypes.STRING,
+                AddUser: DataTypes.STRING,
+                Pin: DataTypes.STRING,
+                PinGenTime: DataTypes.DATE,
+                TryCount: DataTypes.INTEGER,
+                TransInternalFC: DataTypes.INTEGER,
+                TransExternalFC: DataTypes.INTEGER,
+                TransConferenceFC: DataTypes.INTEGER,
+                TransGroupFC: DataTypes.INTEGER,
+                UpdateUser: DataTypes.STRING
+
         }
     );
 

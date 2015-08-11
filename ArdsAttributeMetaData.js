bff06931d464b80abb6400c7f69d45d301cea619
heshanindika <k.heshan.indika@gmail.com>
2015-08-11 09:18:12 +0530

    remove unique key
    ArdsAttributeMetaData.js

--- ArdsAttributeMetaData.js
+++ ArdsAttributeMetaData.js
@@ -4,11 +4,11 @@
 module.exports = function(sequelize, DataTypes) {
     var ArdsAttributeMetadata = sequelize.define('ARDS_AttributeMetaData', {
 
-            Tenant: {type:DataTypes.INTEGER,unique: 'AttributeMetaDataIndex'},
-            Company: {type:DataTypes.INTEGER,unique: 'AttributeMetaDataIndex'},
-            AttributeClass: {type:DataTypes.STRING,unique: 'AttributeMetaDataIndex'},
-            AttributeType: {type:DataTypes.STRING,unique: 'AttributeMetaDataIndex'},
-            AttributeCategory:{type:DataTypes.STRING,unique: 'AttributeMetaDataIndex'},
+            Tenant: DataTypes.INTEGER,
+            Company: DataTypes.INTEGER,
+            AttributeClass: DataTypes.STRING,
+            AttributeType: DataTypes.STRING,
+            AttributeCategory:DataTypes.STRING,
             WeightPrecentage: DataTypes.FLOAT,
             AttributeMetaDataId:{type:DataTypes.INTEGER, primaryKey:true,autoIncrement: true}
 

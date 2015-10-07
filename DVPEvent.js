b20834df936ade027a098acd9e21a78486914408
dinusha_dck <dinusha@duosoftware.com>
2015-10-07 12:17:41 +0530

    increase event params size

--- DVPEvent.js
+++ DVPEvent.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
             EventCategory: DataTypes.STRING,
             EventTime: DataTypes.DATE,
             EventData: DataTypes.STRING,
-            EventParams: DataTypes.STRING(10000)
+            EventParams: DataTypes.STRING(100000)
         }
     );
 

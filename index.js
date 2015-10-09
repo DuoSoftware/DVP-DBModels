ed8243b4442836e90ea7f89e7deef3571f743c67
dinusha_dck <dinusha@duosoftware.com>
2015-10-09 14:55:14 +0530

    change naming to dependants

--- index.js
+++ index.js
@@ -244,8 +244,8 @@ models.forEach(function(model) {
     m.Image.hasMany(m.Volume, {as: "SystemVolumes"});
     m.Volume.belongsTo(m.Image, {as: "SystemVolumes"});
 
-    m.Image.belongsToMany(m.Image, {as: "Dependant", through: "CSDB_ImageDependance",foreignKey: "DependantId"});
-    m.Image.belongsToMany(m.Image, {as: "DepParent", through: "CSDB_ImageDependance",foreignKey: "ImageId"});
+    m.Image.belongsToMany(m.Image, {as: "Dependants", through: "CSDB_ImageDependance",foreignKey: "DependantId"});
+    m.Image.belongsToMany(m.Image, {as: "DepParents", through: "CSDB_ImageDependance",foreignKey: "ImageId"});
 
     m.Template.belongsToMany(m.Image, {as: "TemplateImage", through: "CSDB_TemplateImage"});
 

db46abf84e990e0320cdd7d0380d4680da955f45
Pawan Sasanka <pawan@duosoftware.com>
2016-07-15 18:00:14 +0530

    Add fields to cron table

--- Cron.js
+++ Cron.js
@@ -7,7 +7,10 @@ module.exports = function(sequelize, DataTypes) {
             Description: DataTypes.STRING,
             CronePattern: DataTypes.STRING,
             CallbackURL: DataTypes.STRING,
-            CallbackData: DataTypes.STRING
+            CallbackData: DataTypes.STRING,
+            Reference:DataTypes.STRING,
+            Company:DataTypes.INTEGER,
+            Tenant:DataTypes.INTEGER
         }
     );
 

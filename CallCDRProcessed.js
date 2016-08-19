c349923f8f043e91ada956f1f4a0b7bcbee9cc2a
dinusha_dck <dinusha@duosoftware.com>
2016-08-19 12:43:20 +0530

    add cdr processed model

---
+++ CallCDRProcessed.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes) {
+    var CallCDRProcessed = sequelize.define('CSDB_CallCDRProcessed',
+        {
+            id: {type: DataTypes.BIGINT, autoIncrement: true},
+            Uuid: {type: DataTypes.STRING, primaryKey: true},
+            CallUuid: DataTypes.STRING,
+            BridgeUuid: DataTypes.STRING,
+            SipFromUser: DataTypes.STRING,
+            SipToUser: DataTypes.STRING,
+            HangupCause: DataTypes.STRING,
+            SwitchName: DataTypes.STRING,
+            CallerContext: DataTypes.STRING,
+            IsAnswered: DataTypes.BOOLEAN,
+            CreatedTime: DataTypes.DATE,
+            AnsweredTime: DataTypes.DATE,
+            BridgedTime: DataTypes.DATE,
+            HangupTime: DataTypes.DATE,
+            Duration: DataTypes.INTEGER,
+            AgentAnswered: DataTypes.BOOLEAN,
+            BillSec: DataTypes.INTEGER,
+            HoldSec: DataTypes.INTEGER,
+            QueueSec: DataTypes.INTEGER,
+            ProgressSec: DataTypes.INTEGER,
+            AnswerSec: DataTypes.INTEGER,
+            WaitSec: DataTypes.INTEGER,
+            ProgressMediaSec: DataTypes.INTEGER,
+            FlowBillSec: DataTypes.INTEGER,
+            ExtraData: DataTypes.STRING(1000),
+            DVPCallDirection: DataTypes.STRING,
+            ObjType: DataTypes.STRING,
+            ObjCategory: DataTypes.STRING,
+            AppId: DataTypes.INTEGER,
+            AgentSkill: DataTypes.STRING,
+            CompanyId: DataTypes.INTEGER,
+            TenantId: DataTypes.INTEGER
+        }
+
+    );
+
+
+    return CallCDRProcessed;
+};

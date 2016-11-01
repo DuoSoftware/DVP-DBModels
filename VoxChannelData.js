39483444df1af5323c3010c7ec88b1dbb4991ec8
waruna <waruna@duosoftware.com>
2016-11-01 11:00:07 +0530

    add vox data table

---
+++ VoxChannelData.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Rajinda on 9/29/2015.
+ */
+
+
+module.exports = function (sequelize, DataTypes) {
+    var VoxChannelData = sequelize.define('DB_VOX_ChannelData', {
+            ChannelCount : DataTypes.INTEGER,
+            CapacityGroupId : DataTypes.STRING,
+            TenantId:  {type:DataTypes.INTEGER},
+            CompanyId:  {type:DataTypes.INTEGER},
+            Status: DataTypes.BOOLEAN,
+            ChannelDataId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+        }
+    );
+    return VoxChannelData;
+};
---
+++ VoxMasterData.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Rajinda on 9/29/2015.
+ */
+
+
+module.exports = function (sequelize, DataTypes) {
+    var VoxMasterData = sequelize.define('DB_VOX_MasterData', {
+            ChannelCount : DataTypes.INTEGER,
+            AvailableChannelCount : DataTypes.INTEGER,
+            Status: DataTypes.BOOLEAN,
+            MasterDataId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+        }
+    );
+    return VoxMasterData;
+};

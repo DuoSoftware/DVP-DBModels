aa85d6965fc937cbcf042caf83cd0d04a1c2fed4
waruna@duosoftware.com <waruna@duosoftware.com>
2016-11-29 14:35:27 +0530

    add new fields

--- WalletHistory.js
+++ WalletHistory.js
@@ -5,15 +5,18 @@
 
 module.exports = function (sequelize, DataTypes) {
     var WalletHistory = sequelize.define('DB_WalletHistory', {
-            StripeId : DataTypes.STRING,
-            Description : DataTypes.STRING,
+            StripeId: DataTypes.STRING,
+            Description: DataTypes.STRING,
             OtherJsonData: DataTypes.JSON,
             CurrencyISO: DataTypes.STRING,
-            Tag : {type:DataTypes.ARRAY(DataTypes.STRING)} ,
+            Tag: {type: DataTypes.ARRAY(DataTypes.STRING)},
             Credit: DataTypes.DECIMAL,
-            TenantId:  {type:DataTypes.INTEGER},
-            CompanyId:  {type:DataTypes.INTEGER},
-            WalletId:  {type:DataTypes.INTEGER},
+            TenantId: {type: DataTypes.INTEGER},
+            CompanyId: {type: DataTypes.INTEGER},
+            WalletId: {type: DataTypes.INTEGER},
+            Operation: DataTypes.STRING,
+            InvokeBy: DataTypes.STRING,
+            Reason: DataTypes.STRING,
             WalletHistoryId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
         }
     );

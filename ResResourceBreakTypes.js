1fcbb962daf4e74e7f5641de1ab95a1f8aa0cf6d
heshanindika <k.heshan.indika@gmail.com>
2017-02-06 11:32:00 +0530

    add composite key to ResResourceBreakTypes.js (Tenant, Company, BreakType)

--- ResResourceBreakTypes.js
+++ ResResourceBreakTypes.js
@@ -6,9 +6,9 @@
 module.exports = function (sequelize, DataTypes) {
     var ResResourceBreakTypes = sequelize.define('DB_RES_ResourceBreakTypes', {
             RecordId:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-            TenantId:  {type:DataTypes.INTEGER},
-            CompanyId: {type:DataTypes.INTEGER},
-            BreakType: {type:DataTypes.STRING},
+            TenantId:  {type:DataTypes.INTEGER, unique: 'BreakTypeIndex'},
+            CompanyId: {type:DataTypes.INTEGER, unique: 'BreakTypeIndex'},
+            BreakType: {type:DataTypes.STRING, unique: 'BreakTypeIndex'},
             Active: {type:DataTypes.STRING}
         }
     );

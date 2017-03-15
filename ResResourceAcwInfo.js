5dc5818fbd4eaa4bfd83c02fefa564847ff4faaa
heshanindika <k.heshan.indika@gmail.com>
2017-03-15 16:30:56 +0530

    fix table name

--- ResResourceAcwInfo.js
+++ ResResourceAcwInfo.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = function (sequelize, DataTypes) {
-    var ResResourceAcwInfo = sequelize.define('DB_RES_ResourceStatusDurationInfo', {
+    var ResResourceAcwInfo = sequelize.define('DB_RES_ResourceAcwInfo', {
             RecordId:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
             TenantId:  {type:DataTypes.INTEGER},
             CompanyId: {type:DataTypes.INTEGER},

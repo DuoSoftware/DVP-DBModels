076d8520a608c17338304872051f2f3a2ae11b2a
Pawan Sasanka <pawan@duosoftware.com>
2017-06-14 16:27:24 +0530

    Size changed

--- PersistenceMessages.js
+++ PersistenceMessages.js
@@ -7,7 +7,7 @@ module.exports = function(sequelize, DataTypes) {
             From: DataTypes.STRING,
             To: DataTypes.STRING,
             Time: DataTypes.DATE,
-            Callback :DataTypes.STRING(1000),
+            Callback :DataTypes.STRING(20000),
             CompanyId: DataTypes.INTEGER,
             TenantId: DataTypes.INTEGER
 

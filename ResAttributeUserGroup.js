1e10d654e11ab8b2f8abf1bae8a696f53eafb48d
dinushadck <dinusha@duosoftware.com>
2018-09-20 15:05:44 +0530

    add cascade on delete

--- ResAttributeUserGroup.js
+++ ResAttributeUserGroup.js
@@ -2,7 +2,7 @@ module.exports = function (sequelize, DataTypes) {
     var ResAttributeUserGroup = sequelize.define('DB_RES_Attribute_User_Group', {
             GroupId: {type:DataTypes.STRING, unique: 'ResAttributeGroupCompositeIndex'},
             GroupName: DataTypes.STRING,
-            AttributeId: {type:DataTypes.INTEGER, unique: 'ResAttributeGroupCompositeIndex'}
+            AttributeId: {type:DataTypes.INTEGER, unique: 'ResAttributeGroupCompositeIndex', onDelete: "cascade"}
         }
     );
     return ResAttributeUserGroup;

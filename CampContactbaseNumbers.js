a9ab92ea2bd8fd8bcd44fcd7fe87198c472b47bc
waruna <waruna@duosoftware.com>
2019-01-02 18:08:09 +0530

    add new table

--- CampContactbaseNumbers.js
+++ CampContactbaseNumbers.js
@@ -6,12 +6,14 @@
 module.exports = function(sequelize, DataTypes) {
     var CampContactbaseNumbers = sequelize.define('DB_CAMP_Contactbase_numbers', {
             ExternalUserID: {type: DataTypes.STRING, unique: 'CampContactbaseNumbersIndex'},
-        CampaignId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
+            CampaignId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
             Status: DataTypes.BOOLEAN,
             TenantId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
             CompanyId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
             BatchNo: {type: DataTypes.STRING},
-            CamContactBaseNumberId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+            PreviewData: {type: DataTypes.STRING},
+            CamContactBaseNumberId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+            DialerStatus : {unique: 'CampContactbaseNumbersIndex', type: DataTypes.ENUM, values: ['pick', 'dial', 'processing','added']}
         }
     );
     return CampContactbaseNumbers;

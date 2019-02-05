bce1f546a2c6e3ded232f17ed125a822ba8654ad
dinushadck <dinusha@duosoftware.com>
2019-02-05 10:46:19 +0530

    remove composite key CampContactbaseNumbers.js

--- CampContactbaseNumbers.js
+++ CampContactbaseNumbers.js
@@ -5,15 +5,15 @@
 
 module.exports = function(sequelize, DataTypes) {
     var CampContactbaseNumbers = sequelize.define('DB_CAMP_Contactbase_numbers', {
-            ExternalUserID: {type: DataTypes.STRING, unique: 'CampContactbaseNumbersIndex'},
-            CampaignId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
+            ExternalUserID: {type: DataTypes.STRING},
+            CampaignId: {type: DataTypes.INTEGER},
             Status: DataTypes.BOOLEAN,
-            TenantId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
-            CompanyId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
+            TenantId: {type: DataTypes.INTEGER},
+            CompanyId: {type: DataTypes.INTEGER},
             BatchNo: {type: DataTypes.STRING},
             PreviewData: {type: DataTypes.TEXT},
             CamContactBaseNumberId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-            DialerStatus : {unique: 'CampContactbaseNumbersIndex', type: DataTypes.ENUM, values: ['pick', 'dial', 'processing','added']}
+            DialerStatus : {type: DataTypes.ENUM, values: ['pick', 'dial', 'processing','added']}
         }
     );
     return CampContactbaseNumbers;

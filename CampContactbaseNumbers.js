51e60d01d2af9ea8d7e553af68b7ba34b3426ee1
waruna <waruna@duosoftware.com>
2019-03-04 11:32:52 +0530

    save CamScheduleId with contact no upload

--- CampContactbaseNumbers.js
+++ CampContactbaseNumbers.js
@@ -5,15 +5,16 @@
 
 module.exports = function(sequelize, DataTypes) {
     var CampContactbaseNumbers = sequelize.define('DB_CAMP_Contactbase_numbers', {
-            ExternalUserID: {type: DataTypes.STRING},
-            CampaignId: {type: DataTypes.INTEGER},
+            ExternalUserID: {type: DataTypes.STRING, unique: 'CampContactbaseNumbersIndex'},
+            CampaignId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
             Status: DataTypes.BOOLEAN,
-            TenantId: {type: DataTypes.INTEGER},
-            CompanyId: {type: DataTypes.INTEGER},
+            TenantId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
+            CompanyId: {type: DataTypes.INTEGER, unique: 'CampContactbaseNumbersIndex'},
             BatchNo: {type: DataTypes.STRING},
             PreviewData: {type: DataTypes.TEXT},
             CamContactBaseNumberId: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-            DialerStatus : {type: DataTypes.ENUM, values: ['pick', 'dial', 'processing','added']}
+            DialerStatus : {unique: 'CampContactbaseNumbersIndex', type: DataTypes.ENUM, values: ['pick', 'dial', 'processing','added']},
+            CamScheduleId: {type:DataTypes.INTEGER,unique: 'CampContactbaseNumbersIndex'}
         }
     );
     return CampContactbaseNumbers;

f751ca80e8727b8ec9a23f292b02e86feb56815a
coder a <duoacoder@gmail.com>
2020-01-25 22:10:05 +0530

    sequalize version updated

--- config/default.js
+++ config/default.js
@@ -1,31 +1,29 @@
 module.exports = {
-  "DB": {
-      "Type": "postgres",
-      "User": "duo",
-      "Password": "DuoS123",
-      "Port": 5432,
-      "Host": "104.236.231.11",
-      "Database": "duo",
-      "Cluster": true,
-      "dialectOptions":{
-          "keepAlive": false,
-          "statement_timeout": 10000
-      }
+  DB: {
+    Type: "postgres",
+    User: "duo",
+    Password: "DuoS123",
+    Port: 5432,
+    Host: "ec2-3-15-222-85.us-east-2.compute.amazonaws.com",
+    Database: "duo",
+    Cluster: true,
+    dialectOptions: {
+      keepAlive: false,
+      statement_timeout: 10000
+    }
   }
 
-    // "DB": {
-    //     "Type": "mssql",
-    //     "User": "sa",
-    //     "Password": "DuoSql@2017",
-    //     "Port": 1433,
-    //     "Host": "104.236.61.130",
-    //     "Database": "auth",
-    //     "Cluster": true,
-    //     "dialectOptions":{
-    //         "keepAlive": false,
-    //         "statement_timeout": 10000
-    //     }
-    // }
-
-
+  // "DB": {
+  //     "Type": "mssql",
+  //     "User": "sa",
+  //     "Password": "DuoSql@2017",
+  //     "Port": 1433,
+  //     "Host": "104.236.61.130",
+  //     "Database": "auth",
+  //     "Cluster": true,
+  //     "dialectOptions":{
+  //         "keepAlive": false,
+  //         "statement_timeout": 10000
+  //     }
+  // }
 };
